51fbae02055009025fdc6a99e07ffe832c4b90ed
GNUfault <bluMATRIKZ@gmail.com>
2025-10-16 16:13:44 -0700

    thingys

--- linux/include/generated/compile.h
+++ linux/include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 4 */
 #define UTS_MACHINE "i386"
-#define UTS_VERSION "#3 Thu Oct 16 15:42:26 PDT 2025"
+#define UTS_VERSION "#4 Thu Oct 16 16:13:30 PDT 2025"
 #define LINUX_COMPILE_BY "connor"
 #define LINUX_COMPILE_HOST "Void-T480s"
 #define LINUX_COMPILER "gcc (GCC) 14.2.1 20250405, GNU ld (GNU Binutils) 2.44"
